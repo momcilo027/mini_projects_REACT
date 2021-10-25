7d6d5a02b6498c58e97c6b0e4831f4fc06285193
unknown <krstic.m.027@gmail.com>
2021-10-26 00:40:00 +0200

    code

--- src/App.js
+++ src/App.js
@@ -60,9 +60,63 @@ function App() {
       setBoard([...board])
     }
   }
+  // end of TIC TAC Toe
+
+  // CODE cracker
+
+  const [code, setCode] = useState(Array(4).fill(null));
+  const [enteredValue, setEnteredValue] = useState(0);
+  const [hint, setHint] = useState('test');
+  const [generated, setGenerated] = useState(false);
+
+  const generate_code = () =>{
+    var num1 = Math.floor(Math.random() * (0, 10))
+    var num2 = Math.floor(Math.random() * (0, 10))
+    var num3 = Math.floor(Math.random() * (0, 10))
+    var num4 = Math.floor(Math.random() * (0, 10))
+    setCode(code[0] = num1)
+    setCode(code[1] = num2)
+    setCode(code[2] = num3)
+    setCode(code[3] = num4)
+    setCode([...code])
+    setGenerated(!generated)
+    console.log(code)
+  }
+
+  const try_and_hints = () =>{
+    var code1 = code[0]
+    var code2 = code[1]
+    var code3 = code[2]
+    var code4 = code[3]
+
+    var vals = enteredValue.toString().split('').map(val => parseInt(val, 10))
+    var val1 = vals[0]
+    var val2 = vals[1]
+    var val3 = vals[2]
+    var val4 = vals[3]
+
+    if(code1 == val1){
+
+    }
+
+  }
+
+  // end of CODE cracker
 
   return (
     <div className="App">
+      <div className="pw">
+        <h1>Crack the code...</h1>
+        <h1>X - X - X - X</h1>
+        <button className="generate" onClick={generated ? null : generate_code}>Generate</button> <br/>
+        <input id="code" name='code' className="code" placeholder="XXXX" maxlength="4" onChange={event => setEnteredValue(event.target.value)}/> <br/>
+        <button className="try" onClick={try_and_hints}>TRY</button>
+        <div>
+          <h1>HINTS</h1>
+          <h3>{hint}</h3>
+        </div>
+      </div>
+      <hr />
       <div className="tic_tac_toe">
         <h1>Tic Tac Toe</h1>
         <div className="score">
@@ -76,7 +130,7 @@ function App() {
         </div>
         <div className="board">
           {board.map((value, i) => (
-            <button key={i} id={i} className="board_btn" onClick={(winner == '-' || winner == null) ? handleClick : null}>{value}</button>
+            <button key={i} id={i} className="board_btn" onClick={(value == '-' && ( winner == '-' || winner == null )) ? handleClick : null}>{value}</button>
           ))}
         </div>
       </div>
